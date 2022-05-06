a12a81340fe59f951220509331efd9b7c3a5d021
magynhard <github@mail.magynhard.de>
2022-05-06 15:05:11 +0200

    refactor doc generation

--- scripts/doc.js
+++ scripts/doc.js
@@ -3,35 +3,41 @@
  */
 const fs = require("fs");
 const {exec} = require("child_process");
+const {execSync} = require("child_process");
 
+const LuckyCase = require("lucky-case/string");
+
+require('./../src/ruby-nice/array');
+
+const files_to_doc = [
+    './src/ruby-nice/ruby-nice.js',
+    './src/ruby-nice/string.js',
+    './src/ruby-nice/file.js',
+]
 
 function generateDoc() {
-    const ruby_nice_doc_file = './doc/ruby-nice.jsdoc.md';
-    exec(`./node_modules/jsdoc-to-markdown/bin/cli.js --files ./src/ruby-nice/ruby-nice.js > ${ruby_nice_doc_file}`, (error, stdout, stderr) => {
-        if (error) {
-            console.log(`error: ${error.message}`);
-        }
-        if (stderr) {
-            console.log(`stderr: ${stderr}`);
-        }
-        beautifyDoc(ruby_nice_doc_file);
-        const ruby_nice_string_doc_file = './doc/ruby-nice-string.jsdoc.md';
-        exec(`./node_modules/jsdoc-to-markdown/bin/cli.js --files ./src/ruby-nice/string.js > ${ruby_nice_string_doc_file}`, (error, stdout, stderr) => {
-            beautifyDoc(ruby_nice_string_doc_file);
-        });
-    });
+    for(let source_file of files_to_doc) {
+        const doc_file = './doc/' + source_file.split('/').last().replace(/\.js$/gm,'.jsdoc.md');
+        execSync(`./node_modules/jsdoc-to-markdown/bin/cli.js --files ${source_file} > ${doc_file}`);
+        beautifyDoc(doc_file);
+    }
 }
 
 function beautifyDoc(file) {
     const kind_line_regex = /^[\n\r]\*\*Kind[^\n\r]*[\n\r]/gms;
     const arrow_right_char_regex = /⇒/g;
-    const function_description_regex = /## Classes.*<a name="String"><\/a>/gms;
-    const functions_regex = /<a name="downcase"><\/a>.*/gms;
+    const class_name = file.split('/').last().split('.').first().toPascalCase();
+    const function_description_regex = new RegExp(`## Classes.*<a name="${class_name}"><\/a>`, 'gms');
     let data = fs.readFileSync(file, 'utf-8').toString();
+    const first_method_name_match = data.match((new RegExp(`<a name="${class_name}\\+([^"]+)"><\/a>`,'')));
+    const first_method_name = first_method_name_match && first_method_name_match[1] ? first_method_name_match[1] : null;
     data = data.replace(kind_line_regex,'');
     data = data.replace(arrow_right_char_regex,'&rarr;');
     data = data.replace(function_description_regex,'<a name="String"></a>');
-    data = data.replace(functions_regex,'');
+    if(first_method_name) {
+        const functions_regex = new RegExp(`<a name="${first_method_name}"><\/a>.*`,'gms');
+        data = data.replace(functions_regex,'');
+    }
     fs.writeFileSync(file, data, 'utf-8');
 }
 
