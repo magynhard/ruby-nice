cb0e45c94bdd92e63f04d004fc103a7c12911afa
magynhard <github@mail.magynhard.de>
2022-05-20 17:57:40 +0200

    add 'FileUtils.copy()'

--- doc/file-utils.jsdoc.md
+++ doc/file-utils.jsdoc.md
@@ -6,8 +6,20 @@ FileUtils class port of ruby
 For node js only, does not work inside a browser.
 
 * [FileUtils](#FileUtils)
+    * [.copy(src, dest, mode)](#FileUtils.copy)
     * [.mkdirP(file_name)](#FileUtils.mkdirP)
     * [.rmRf(file_name, opt)](#FileUtils.rmRf)
+
+<a name="FileUtils.copy"></a>
+
+### FileUtils.copy(src, dest, mode)
+Copy file
+
+| Param | Type | Description |
+| --- | --- | --- |
+| src | <code>string</code> \| <code>Array.&lt;string&gt;</code> | path(s) to source file(s) |
+| dest | <code>string</code> | path to destination |
+| mode | <code>number</code> \| <code>fs.constants.COPYFILE\_EXCL</code> \| <code>fs.constants.COPYFILE\_FICLONE</code> \| <code>fs.constants.COPYFILE\_FICLONE\_FORCE</code> | specify behaviour of copy operation |
 
 <a name="FileUtils.mkdirP"></a>
 
--- src/ruby-nice/file-utils.js
+++ src/ruby-nice/file-utils.js
@@ -5,6 +5,7 @@ if (typeof require === 'function' && typeof module !== 'undefined' && module.exp
     var Mime = require('mime');
     var RubyNice = require('./ruby-nice');
     var Typifier = require('typifier');
+    var File = require('./file');
     require('./array');
 }
 //<!-- /MODULE -->//
@@ -22,6 +23,29 @@ if (typeof require === 'function' && typeof module !== 'undefined' && module.exp
 class FileUtils {
 
     /**
+     * Copy file
+     *
+     * @param {string|Array<string>} src path(s) to source file(s)
+     * @param {string} dest path to destination
+     * @param {number|fs.constants.COPYFILE_EXCL|fs.constants.COPYFILE_FICLONE|fs.constants.COPYFILE_FICLONE_FORCE} mode specify behaviour of copy operation
+     */
+    static copy(src, dest, mode) {
+        const self = FileUtils;
+        RubyNice.ensureRunningInNodeJs();
+        if(Typifier.isArray(src)) {
+            if(File.isDirectory(dest)) {
+                src.each((file, index) => {
+                   fs.copyFileSync(file, dest, mode);
+                });
+            } else {
+                throw new Error(`Parameter 'src' is a array of files. Then the parameter 'dest' must be a valid directory! Directory not found: '${dest}'`);
+            }
+        } else {
+            fs.copyFileSync(src, dest, mode);
+        }
+    }
+
+    /**
      * Create directory recursively
      *
      * @param {string|Array<string>} file_name path(s) to create
@@ -36,6 +60,7 @@ class FileUtils {
             Fs.mkdirSync(path, { recursive: true });
         });
     }
+
     /**
      * Deletes directory recursively including its contents
      *
