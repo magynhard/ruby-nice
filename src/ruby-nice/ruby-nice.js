dc6e8858580bb128ce18e1a3375550f70014e254
magynhard <github@mail.magynhard.de>
2022-05-20 18:10:09 +0200

    create release 0.0.30 - add feature to monkey patch all at once - use generic exports pattern

--- src/ruby-nice/ruby-nice.js
+++ src/ruby-nice/ruby-nice.js
@@ -53,7 +53,7 @@ class RubyNice {
  * @type {string}
  * @private
  */
-RubyNice._version = "0.0.29";
+RubyNice._version = "0.0.30";
 
 //<!-- MODULE -->//
 if (typeof module !== 'undefined' && module.exports) {
