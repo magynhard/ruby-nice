9ac53a8f2d2749cdf08c80835ae4d28c093eab08
magynhard <github@mail.magynhard.de>
2022-05-20 18:12:42 +0200

    create release 0.0.32 - fix typo

--- src/ruby-nice/ruby-nice.js
+++ src/ruby-nice/ruby-nice.js
@@ -53,7 +53,7 @@ class RubyNice {
  * @type {string}
  * @private
  */
-RubyNice._version = "0.0.31";
+RubyNice._version = "0.0.32";
 
 //<!-- MODULE -->//
 if (typeof module !== 'undefined' && module.exports) {
